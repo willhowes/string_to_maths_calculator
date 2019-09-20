d964abc51d2ba1a606e4ad14ebcb2f10251fa0aa
Will Howes <williameldenhowes@gmail.com>
2019-09-20 14:25:21 +0100

    Test for a second subtraction sum passes

--- lib/maths_evaluator.rb
+++ lib/maths_evaluator.rb
@@ -3,7 +3,12 @@ def maths_evaluator(sum_as_string)
     return ["1 + 1", 2]
   end
   if sum_as_string.split('').include?('-')
-    return ['1 - 1', 0]
+    numbers = []
+    sum_as_string.delete('^0-9').split('').each do |number|
+      numbers << number
+    end
+    result = numbers[0].to_i - numbers[1].to_i
+    return ["#{sum_as_string}", result]
   end
   ["1 + 2", 3]
 end
--- spec/maths_evaluator_spec.rb
+++ spec/maths_evaluator_spec.rb
@@ -12,4 +12,8 @@ describe '#maths_evaluator' do
   it "Returns '[1 - 1, 0]' for an input of '1 - 1'" do
     expect(maths_evaluator("1 - 1")).to eq(['1 - 1', 0])
   end
+
+  it "Returns '[2 - 1, 1]' for an input of '2 - 1'" do
+    expect(maths_evaluator("2 - 1")).to eq(['2 - 1', 1])
+  end
 end
