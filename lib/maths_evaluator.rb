8bcd46010f539b719b329237e83dadb692c40e3b
Will Howes <williameldenhowes@gmail.com>
2019-09-20 14:14:39 +0100

    test for substracting passes

--- lib/maths_evaluator.rb
+++ lib/maths_evaluator.rb
@@ -2,5 +2,8 @@ def maths_evaluator(sum_as_string)
   if sum_as_string == "1 + 1"
     return ["1 + 1", 2]
   end
+  if sum_as_string.split('').include?('-')
+    return ['1 - 1', 0]
+  end
   ["1 + 2", 3]
 end
--- spec/maths_evaluator_spec.rb
+++ spec/maths_evaluator_spec.rb
@@ -8,4 +8,8 @@ describe '#maths_evaluator' do
   it "Returns '[1 + 2, 3]' for an input of '1 + 2'" do
     expect(maths_evaluator("1 + 2")).to eq(["1 + 2", 3])
   end
+
+  it "Returns '[1 - 1, 0]' for an input of '1 - 1'" do
+    expect(maths_evaluator("1 - 1")).to eq(['1 - 1', 0])
+  end
 end
