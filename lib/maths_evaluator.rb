25544b40cd2ef33488f9858bbef3d9beb14620b9
Will Howes <williameldenhowes@gmail.com>
2019-09-20 15:51:52 +0100

    Refactored to use case expression

--- lib/maths_evaluator.rb
+++ lib/maths_evaluator.rb
@@ -1,17 +1,19 @@
 def maths_evaluator(sum_as_string)
   split_sum = sum_as_string.split(' ')
-  if split_sum.include?('+')
-    result = split_sum[0].to_i + split_sum[2].to_i
-  end
-  if sum_as_string.split('').include?('-')
-    result = split_sum[0].to_i - split_sum[2].to_i
-  end
-  if split_sum.include?('*')
-    result = split_sum[0].to_i * split_sum[2].to_i
-  end
 
-  if split_sum.include?('/')
+  case split_sum[1]
+
+  when '+'
+    result = split_sum[0].to_i + split_sum[2].to_i
+
+  when '-'
+    result = split_sum[0].to_i - split_sum[2].to_i
+
+  when '*'
+    result = split_sum[0].to_i * split_sum[2].to_i
+
+  when '/'
     result = split_sum[0].to_i / split_sum[2].to_i
   end
-  ["#{sum_as_string}", result]
+  return ["#{sum_as_string}", result]
 end
