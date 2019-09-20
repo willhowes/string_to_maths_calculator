50e065c2de3b2c6f1167db19d27f152fa04dfbc7
Will Howes <williameldenhowes@gmail.com>
2019-09-20 15:39:07 +0100

    Refactored out the each iterator and now unecessary separate addition method

--- lib/maths_evaluator.rb
+++ lib/maths_evaluator.rb
@@ -1,31 +1,13 @@
-private
-def handle_addition(sum)
-  numbers = []
-  sum.delete('^0-9').split('').each do |number|
-    numbers << number
-  end
-  result = numbers[0].to_i + numbers[1].to_i
-  return ["#{sum}", result]
-end
-
 def maths_evaluator(sum_as_string)
-  numbers = []
-
-  sum_as_string_as_array = sum_as_string.split('')
-  if sum_as_string_as_array.include?('+')
-    return handle_addition(sum_as_string)
+  split_sum = sum_as_string.split(' ')
+  if split_sum.include?('+')
+    result = split_sum[0].to_i + split_sum[2].to_i
   end
   if sum_as_string.split('').include?('-')
-    sum_as_string.delete('^0-9').split('').each do |number|
-      numbers << number
-    end
-    result = numbers[0].to_i - numbers[1].to_i
+    result = split_sum[0].to_i - split_sum[2].to_i
   end
-  if sum_as_string_as_array.include?('*')
-    sum_as_string.delete('^0-9').split('').each do |number|
-      numbers << number
-    end
-    result = numbers[0].to_i * numbers[1].to_i
+  if split_sum.include?('*')
+    result = split_sum[0].to_i * split_sum[2].to_i
   end
-  return ["#{sum_as_string}", result]
+  ["#{sum_as_string}", result]
 end
