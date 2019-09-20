c4f7f6f65b305659ab6cc9b984a9d98ba8826cef
Will Howes <williameldenhowes@gmail.com>
2019-09-20 15:29:52 +0100

    Initial refactor. new private method for handling addition

--- lib/maths_evaluator.rb
+++ lib/maths_evaluator.rb
@@ -1,26 +1,31 @@
+private
+def handle_addition(sum)
+  numbers = []
+  sum.delete('^0-9').split('').each do |number|
+    numbers << number
+  end
+  result = numbers[0].to_i + numbers[1].to_i
+  return ["#{sum}", result]
+end
+
 def maths_evaluator(sum_as_string)
-  if sum_as_string.split('').include?('+')
-    numbers = []
-    sum_as_string.delete('^0-9').split('').each do |number|
-      numbers << number
-    end
-    result = numbers[0].to_i + numbers[1].to_i
-    return ["#{sum_as_string}", result]
+  numbers = []
+
+  sum_as_string_as_array = sum_as_string.split('')
+  if sum_as_string_as_array.include?('+')
+    return handle_addition(sum_as_string)
   end
   if sum_as_string.split('').include?('-')
-    numbers = []
     sum_as_string.delete('^0-9').split('').each do |number|
       numbers << number
     end
     result = numbers[0].to_i - numbers[1].to_i
-    return ["#{sum_as_string}", result]
   end
-  if sum_as_string.split('').include?('*')
-    numbers = []
+  if sum_as_string_as_array.include?('*')
     sum_as_string.delete('^0-9').split('').each do |number|
       numbers << number
     end
     result = numbers[0].to_i * numbers[1].to_i
-    return ["#{sum_as_string}", result]
   end
+  return ["#{sum_as_string}", result]
 end
